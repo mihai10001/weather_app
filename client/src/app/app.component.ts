bc357b990aba204d5214564c7b3366b944c026ca
mihai10001 <rvivits@gmail.com>
2021-07-04 11:46:47 +0300

    call weather service and render result HTML

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import { WeatherService } from './weather.service';
+import { weatherObjectModel } from './weather.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  weatherObject: weatherObjectModel | undefined = undefined;
   title = 'client';
+
+  constructor(
+    private weatherService: WeatherService
+  ) { }
+
+
+  getWeather(city: string) {
+    this.weatherService
+      .getWeatherObservable(city)
+      .subscribe(weather => {
+          this.weatherObject = weather as weatherObjectModel;
+        }, (error) => {
+          this.weatherObject = undefined;
+          console.log(error);
+        }
+      );
+  }
 }
